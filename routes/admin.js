1aaa16f3219d80729e483623106fc1f3559c6acf
Alexander <kechko02@gmail.com>
2021-02-11 15:34:38 +0300

    Правка формы в админе

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Admin = require('../model/admin');
 const bcrypt = require('bcrypt');
+const Student = require('../model/student')
 
 router.get('/', (req, res) => {
   return res.render('admin/login', { title: 'Вход' });
@@ -10,16 +11,28 @@ router.get('/', (req, res) => {
 router.post('/', async (req, res) => {
   const { login, password } = req.body;
   const adminUser = await Admin.findOne({ login });
+  console.log(adminUser);
 
   if (!adminUser || !(await bcrypt.compare(password, adminUser?.password))) {
     return res.redirect('/admin');
   }
 
-  req.session.AdminID = adminUser._id;
+  const students = await Student.find();
 
-  return res.render('admin/studentList', { title: 'список студентов' });
+  req.session.AdminID = adminUser._id;
+  return res.render('admin/adminList', { title: 'список студентов', students });
 
 });
+
+// router.get('/:id')
+
+router.get('/filterbyhowknow', async (req, res) => {
+  const filterParams = req.body;
+  // const filterDB = await Student.find({ reason: 'Всегда мечтал(а) стать разработчиком' });
+  const filterDB = await Student.find().sort('lastName');
+  console.log(filterDB);
+  return res.render('admin/studentList', { title: 'список студентов', list: filterDB })
+})
 
 
 
