199319c43380d15d2033dd626540018e63a75cd0
Abay <abay.reushenov@gmail.com>
2021-02-12 00:42:25 +0300

    Исправлено форма редактирования студента Test/test.js

--- routes/test/test.js
+++ routes/test/test.js
@@ -4,9 +4,11 @@ const Student = require('../../model/student');
 // const check = require('../middleware/check');
 
 router.get('/', async (req, res) => {
-  const student = await Student.findById('602574a780d732fd74b2b024');
+  let student = await Student.findById('6025a0a99b74c6334a3de129').lean();
   console.log(student);
-  return res.render('admin/profile', { student });
+  const editReceiptDate = new Date(student.receiptDate).toISOString().substring(0, 10)
+  const editbirthday = new Date(student.birthday).toISOString().substring(0, 10);
+  return res.render('admin/profile', { student, editReceiptDate, editbirthday });
 });
 
 module.exports = router;
