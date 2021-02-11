f85a97554d08e521fcdea4859cc3edd3f4dda03d
Abay <abay.reushenov@gmail.com>
2021-02-11 23:03:00 +0300

    Release 23-02 Abay home

--- routes/test/test.js
+++ routes/test/test.js
@@ -4,7 +4,8 @@ const Student = require('../../model/student');
 // const check = require('../middleware/check');
 
 router.get('/', async (req, res) => {
-  const student = await Student.findById('60254a341a8137c56dd62dc1');
+  const student = await Student.findById('602574a780d732fd74b2b024');
+  console.log(student);
   return res.render('admin/profile', { student });
 });
 
