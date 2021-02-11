7e53b166279d8b16d69ffb699de77fd18310beed
Abay <abay.reushenov@gmail.com>
2021-02-11 18:36:48 +0300

    Release Доббавлено route => test/test.js роут отрабатывает hbs => admin/profile.hbs

--- routes/test/test.js
+++ routes/test/test.js
@@ -4,7 +4,7 @@ const Student = require('../../model/student');
 // const check = require('../middleware/check');
 
 router.get('/', async (req, res) => {
-  const student = await Student.findOne();
+  const student = await Student.findById('60254a341a8137c56dd62dc1');
   return res.render('admin/profile', { student });
 });
 
