0fc34f2c317ad9f21c612c3bf4d7ec8186b2a38a
Abay <abay.reushenov@gmail.com>
2021-02-12 13:33:22 +0300

    Release Telegramm Bot

--- routes/student.js
+++ routes/student.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const Student = require('../model/student');
+const ctrlTelegram = require('../api/telegramMsg');
+
 // const Admin = require('../model/admin');
 // const bcrypt = require('bcrypt');
 
@@ -12,11 +14,28 @@ router.get('/students/new', (req, res) => {
 });
 
 router.post('/students', async (req, res) => {
-  let reasons = [req.body.reason1, req.body.reason2, req.body.reason3, req.body.reason4, req.body.reason5, req.body.reason6];
-  reasons = reasons.filter(el => el);
-  let howknow = [req.body.howKnow1, req.body.howKnow2, req.body.howKnow3, req.body.howKnow4, req.body.howKnow5, req.body.howKnow6,
-  req.body.howKnow7, req.body.howKnow8, req.body.howKnow9, req.body.howKnow10];
-  howknow = howknow.filter(el => el);
+  let reasons = [
+    req.body.reason1,
+    req.body.reason2,
+    req.body.reason3,
+    req.body.reason4,
+    req.body.reason5,
+    req.body.reason6,
+  ];
+  reasons = reasons.filter((el) => el);
+  let howknow = [
+    req.body.howKnow1,
+    req.body.howKnow2,
+    req.body.howKnow3,
+    req.body.howKnow4,
+    req.body.howKnow5,
+    req.body.howKnow6,
+    req.body.howKnow7,
+    req.body.howKnow8,
+    req.body.howKnow9,
+    req.body.howKnow10,
+  ];
+  howknow = howknow.filter((el) => el);
   const newStudent = new Student({
     email: req.body.email,
     github: req.body.github,
@@ -38,7 +57,6 @@ router.post('/students', async (req, res) => {
 
   // const hashedPasswords = await bcrypt.hash('12345', 10);
 
-
   // const admin = new Admin({
   //   login: 'kechko02@gmail.com',
   //   password: hashedPasswords
@@ -50,12 +68,13 @@ router.post('/students', async (req, res) => {
   } catch (error) {
     return res.render('error', {
       message: 'Не удалось добавить запись в базу данных.',
-      error: error
+      error: error,
     });
   }
-
+  //----------------------------
+  ctrlTelegram.sendMsg(newStudent.firstName, newStudent.lastName, newStudent.number, newStudent.format); 
+  //----------------------------
   return res.render('students/end');
-})
-
+});
 
 module.exports = router;
